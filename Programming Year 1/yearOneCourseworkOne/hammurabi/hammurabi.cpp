7d1bb9e9b4402ad9cfc7036fc55a09370cf659dd
jlform <jordanforeman1@gmail.com>
2024-01-15 15:51:48 +0000

    sync#

---
+++ Programming Year 1/yearOneCourseworkOne/hammurabi/hammurabi.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+/*
+NOTES SECTION
+
+currentYear
+starvationThisYear
+populationIncrease
+population
+acresOwnedByCity
+bushelHarvestPerAcre
+rats
+bushelStorage
+landPrice (17-
+
+buying/selling acres
+how many bushels to feed the people
+how many acres to plant seed on
+*/
+
+
+int currentYear, population, acresOwnedByCity, bushelStorageTotal, bushelHarvestPerAcre, bushelsEatenByRats, landPrice;
+
+int main() 
+{
+	
+}
