ce8c9f2fec54c97aaf4a3fabdc10b03433075939
jlform <jordanforeman1@gmail.com>
2024-03-22 13:28:42 +0000

    project4sync

--- Programming Year 1/yearOneCourseworkTwo/project4/project4.cpp
+++ Programming Year 1/yearOneCourseworkTwo/project4/project4.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
 
 using namespace std;
 
@@ -104,6 +106,10 @@ public:
         this->customerPhoneNumber = customerPhoneNumber;
     }
 
+    string getCustomerName() {
+        return customerName;
+    }
+
     string getCustomerAddress() {
         return customerAddress;
     }
@@ -114,11 +120,13 @@ private:
     Customer customer;
     MenuItem menuitem;
     int orderCounter = 0;
+    static double totalIncome;
 public:
-    Order(string customerName, string customerAddress, string customerPhoneNumber, string itemName, bool availability, double itemPrice, double popularityRating, string cuisine) 
-    : customer(customerName, customerAddress, customerPhoneNumber)
-    , menuitem(itemName, availability, itemPrice, popularityRating, cuisine) {
+    Order(string customerName, string customerAddress, string customerPhoneNumber, string itemName, bool availability, double itemPrice, double popularityRating, string cuisine)
+        : customer(customerName, customerAddress, customerPhoneNumber)
+        , menuitem(itemName, availability, itemPrice, popularityRating, cuisine) {
         this->orderCounter = orderCounter;
+        totalIncome += itemPrice;
     }
     
     bool distanceChecker() {
@@ -135,8 +143,92 @@ public:
         orderCounter++;
     }
 
+    int returnOrderCount() {
+        return orderCounter;
+    }
+
+ 
+    static double getTotalIncome() {
+        return totalIncome;
+    }
+
+    static void resetTotalIncome() {
+        totalIncome = 0.0;
+    }
 };
 
-int main() {
+class Restaurant {
+private:
+    vector<MenuItem> menuItems;
+    vector<Staff> staff;
+    vector<Order> orders;
+    map<string, int> cuisineCount;
+    map<string, int> menuItemCount;
+    map<Customer, int> customerOrderCount;
 
+public:
+    Restaurant(vector<MenuItem> menuItems, vector<Staff> staff) {
+        this->menuItems = menuItems;
+        this->staff = staff;
+    }
+
+    void placeOrder(string customerName, string customerAddress, string customerPhoneNumber, string itemName, bool availability, double itemPrice, double popularityRating, string cuisine) {
+        MenuItem menuItem(itemName, availability, itemPrice, popularityRating, cuisine);
+        Customer customer(customerName, customerAddress, customerPhoneNumber);
+        Order order(customerName, customerAddress, customerPhoneNumber, itemName, availability, itemPrice, popularityRating, cuisine);
+        orders.push_back(order);
+
+        cuisineCount[cuisine]++;
+        menuItemCount[itemName]++;
+        customerOrderCount[customer]++;
+    }
+
+    void generateDailyReport() {
+        cout << "There were " << orders.size() << " orders placed today, generating report: \n";
+        cout << "Total Income for the day was " << Order::getTotalIncome() << "\n";
+
+        string mostPopularCuisine = "";
+        int maxCuisineCount = 0;
+        for (auto& c : cuisineCount) {
+            if (c.second > maxCuisineCount) {
+                maxCuisineCount = c.second;
+                mostPopularCuisine = c.first;
+            }
+        }
+        cout << "Most popular cuisine choice was " << mostPopularCuisine << " with " << maxCuisineCount << " orders.\n";
+
+        string mostPopularMenuItem = "";
+        int maxMenuItemCount = 0;
+        for (auto& m : menuItemCount) {
+            if (m.second > maxMenuItemCount) {
+                maxMenuItemCount = m.second;
+                mostPopularMenuItem = m.first;
+            }
+        }
+        cout << "Most popular menu item was " << mostPopularMenuItem << " with " << maxMenuItemCount << "orders.\n";
+
+        Customer mostLoyalCustomer = Customer("", "", "");
+        int maxCustomerOrderCount = 0;
+        for (auto& c : customerOrderCount) {
+            if (c.second > maxCustomerOrderCount) {
+                maxCustomerOrderCount = c.second;
+                mostLoyalCustomer = c.first;
+            }
+        }
+        cout << "Most loyal customer was " << mostLoyalCustomer.getCustomerName() << " with " << maxCustomerOrderCount << " orders.\n";
+    }
+};
+
+
+int main() {
+    vector<MenuItem> menuItems;
+        // Add menu items to the vector
+    vector<Staff> staff;
+        // Add staff to the vector
+    Restaurant restaurant(menuItems, staff);
+
+    restaurant.placeOrder("John Doe", "CF37", "123456", "Pizza", true, 10.0, 5.0, "Italian");
+    restaurant.placeOrder("Jane Doe", "CF37", "654321", "Pasta", true, 12.0, 4.5, "Italian");
+    restaurant.placeOrder("John Doe", "CF37", "123456", "Rice", false, 5.0, 3.5, "Chinese");
+    restaurant.generateDailyReport();
 }
